2b57a3f66b16343f6ab62213a48012060161f066
M R <6248874+mrbrirup@users.noreply.github.com>
2019-06-04 08:39:15 +0100

    Updating desktop methods and events

--- controls/component.js
+++ controls/component.js
@@ -1,9 +1,12 @@
 class {
     static get inherits() {
         return [
-            "Mrbr.System.Object"
+            "Mrbr.System.Object",
+            "Mrbr.System.EventEmitter"
         ]
     }
+    static get using() { return []; }
+    static get manifest() { return []; }
     constructor(...args) {
         this.base(...args)
     }
--- controls/desktop.js
+++ controls/desktop.js
@@ -3,27 +3,40 @@ class {
     constructor(...args) {
         var self = this;
         self.base(...args)
-        self.app = new PIXI.Application();
-        document.body.appendChild(self.app.view);
-        self.renderer = self.app.renderer;
-        self.desktopStage = new PIXI.Container();
-        self.desktopStage.interactive = true;
-        self.desktopStage.sortableChildren = true;
-        self.animate = self.fnanimate.bind(self);
-        self.animate();
+
+        // self.app = new PIXI.Application();
+        // document.body.appendChild(self.app.view);
+        // self.renderer = self.app.renderer;
+        // self.desktopStage = new PIXI.Container();
+        // self.desktopStage.interactive = true;
+        // self.desktopStage.sortableChildren = true;
+        // self.animate = self.fnanimate.bind(self);
+        // self.animate();
+        this._workingArea = "";
+        this._clientArea = "";
+        this._htmlContainer = "";
+        this._backgroundColour = "";
     }
-    relayerDisplayObjects() {
-        this.displayList.relayerDisplayObjects();
-    }
-    displayObjectSelected(sender, args) {
-        this.displayList.displayObjectSelected(send, args);
-    }
-    addDisplayObject(displayObject) {
-        this.displayList.addDisplayObject(displayObject);
-    }
-    fnanimate() {
-        var self = this;
-        self.renderer.render(self.desktopStage);
-        requestAnimationFrame(self.animate);
-    }
+    get workingArea(){return this._workingArea;}
+    set workingArea(value){this._workingArea = value;}
+    get clientArea(){return this._clientArea;}
+    set clientArea(value){this._clientArea = value;}
+    get htmlContainer(){return this._htmlContainer;}
+    set htmlContainer(value){this._htmlContainer = value;}
+    get backgroundColour(){return this._backgroundColour;}
+    set backgroundColour(value){this._backgroundColour = value;}
+    // relayerDisplayObjects() {
+    //     this.displayList.relayerDisplayObjects();
+    // }
+    // displayObjectSelected(sender, args) {
+    //     this.displayList.displayObjectSelected(send, args);
+    // }
+    // addDisplayObject(displayObject) {
+    //     this.displayList.addDisplayObject(displayObject);
+    // }
+    // fnanimate() {
+    //     var self = this;
+    //     self.renderer.render(self.desktopStage);
+    //     requestAnimationFrame(self.animate);
+    // }
 }
